e414967b4204843e6a7243e3553d4ef685e2b315
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-08-20 22:56:46 +0300

    Add logic for the custom tip input

--- scripts/calculation-results.js
+++ scripts/calculation-results.js
@@ -1,5 +1,6 @@
 const billInputElement = document.querySelector('input.bill-input');
 const percentageButtonElements = document.querySelectorAll('button.percentage-btn');
+const customPercentageInputElement = document.querySelector('input.custom-percentage-input');
 
 
 const tipPercentageIsSelected = () => {}
@@ -8,15 +9,23 @@ const clearAllSelectedButtons = () => {
     percentageButtonElements.forEach(percentageButton => percentageButton.classList.remove('selected-percentage-btn'));
 }
 
+const clearSelectedCustomInput = () => customPercentageInputElement.classList.remove('selected-custom-input');
+
+
 billInputElement.addEventListener('input', () => {
 
 });
-
 
 percentageButtonElements.forEach(percentageButton => {
 
     percentageButton.addEventListener('click', (event) => {
         clearAllSelectedButtons();
+        clearSelectedCustomInput();
         event.target.classList.add('selected-percentage-btn');
     });
+});
+
+customPercentageInputElement.addEventListener('focus', (event) => {
+    clearAllSelectedButtons();
+    event.target.classList.add('selected-custom-input');
 });
